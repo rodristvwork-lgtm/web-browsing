6026b72c4016cd5679635f115c4fdd01db14a384
Rodriguez JHORDAN <rodriguez.jhordan@alten.it>
2026-01-15 16:57:51 +0100

    ma-c4p4: clean phase done

--- modular_software/loading_performance/README.md
+++
@@ -1,2 +0,0 @@
-project related to measurement of webpages loading performance
-launch loading.py from the root of modular_software
--- modular_software/loading_performance/job.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\Users\RemoteAccess\Documents\modular_software
-py .\loading_performance\loading.py
-:loop
-timeout 3600
-py .\loading_performance\loading.py
-goto loop
